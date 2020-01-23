b8941f136dcc382c8d8cd9fd16783815b03082e9
George Mamadashvili <georgemamadashvili@gmail.com>
2020-01-23 16:43:37 +0400

    Update teardown message

--- bin/e2e-tests-teardown.php
+++ bin/e2e-tests-teardown.php
@@ -15,14 +15,14 @@ function tearDown() {
 	$exists = \domain_exists( $current_site->domain, $current_site->path . 'e2e-testing', 1 );
 	if ( $exists ) {
 		\wp_delete_site( $exists );
-		WP_CLI::log( "Deleted site. ID: {$exists}" );
 	}
 
 	$exists = BP_Groups_Group::group_exists( 'e2e-testing-course' );
 	if ( $exists ) {
 		\groups_delete_group( $exists );
-		WP_CLI::log( "Deleted group. ID: {$exists}" );
 	}
+
+	WP_CLI::success( 'The testing data was deleted!' );
 }
 
 tearDown();
